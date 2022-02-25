624af5c2b565c42c4f21dba72dd2fdf0427130ae
AlexsandroD <adrdra88@gmail.com>
2022-02-25 16:50:24 +0100

    push

--- resources/js/guest/components/Main.vue
+++ resources/js/guest/components/Main.vue
@@ -1,26 +1,12 @@
 <template>
     <main>
-        <div class="cards">
-            <div v-for="(post, index) in posts" :key="index" class="card">
-                <h4>{{ post.title }}</h4>
-                <div v-if="post.image">
-                    <img v-bind:src="'../storage/' + post.image" />
-                </div>
-                <div v-else>
-                    <img
-                        src="https://st2.depositphotos.com/1669000/5774/v/600/depositphotos_57749055-stock-illustration-happy-man-is-waiting-for.jpg"
-                        alt=""
-                    />
-                </div>
-                <p>{{ post.content }}</p>
-            </div>
-        </div>
+        <router-view></router-view>
     </main>
 </template>
+
 <script>
 export default {
-    name: "Main"
-
+    name: "Main",
 };
 </script>
 
--- resources/js/guest/pages/Home.vue
+++ resources/js/guest/pages/Home.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <h1>home</h1>
+        <h1>Hone page</h1>
     </div>
 </template>
 
 <script>
 export default {
-    name: "home",
+    name: "Home",
 };
 </script>
 
