63aa4d1393b9571181affe0065d89c5d4fb99f43
AlexsandroD <adrdra88@gmail.com>
2022-03-04 13:39:10 +0100

    implementazione notifica mail

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Comment;
 use Illuminate\Support\Facades\Validator;
+use App\Mail\CommentMail;
+use Illuminate\Support\Facades\Mail;
 
 class CommentController extends Controller
 {
@@ -32,6 +34,10 @@ class CommentController extends Controller
         $newComment->content = $data["content"];
         $newComment->post_id = $data["post_id"];
         $newComment->save();
+
+
+
+        Mail::to('webmaster@boolpress.com')->send(new CommentMail());
 
         return response()->json([
             "success" =>true
---
+++ resources/views/mail/comment.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    Nuovo Commento
+</h1>
